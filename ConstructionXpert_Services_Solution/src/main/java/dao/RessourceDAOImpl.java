60afb54f37cd5b7ce75a4e73fa8ee3436a52ae1d
Reda Ouadi <redaouadi34@gmail.com>
2024-05-23 16:36:01 +0100

    Modification de l'implementataion RessourceDAOImpl

--- ConstructionXpert_Services_Solution/src/main/java/dao/RessourceDAOImpl.java
+++ ConstructionXpert_Services_Solution/src/main/java/dao/RessourceDAOImpl.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import metier.Ressource;
 
-
 public class RessourceDAOImpl implements IRessourceDAO {
 
     private Connection connection;
@@ -19,14 +18,18 @@ public class RessourceDAOImpl implements IRessourceDAO {
 
     @Override
     public Ressource save(Ressource r) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("INSERT INTO ressource(nom, type, quantite, fournisseur) VALUES (?, ?, ?, ?)");
+        String query = "INSERT INTO ressource (nom, type, quantite, fournisseur, id_tache) VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement ps = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, r.getNom());
             ps.setString(2, r.getType());
             ps.setInt(3, r.getQuantite());
             ps.setString(4, r.getFournisseur());
+            ps.setInt(5, r.getId_tache());
             ps.executeUpdate();
-            ps.close();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                r.setId_ressource(rs.getInt(1));
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -36,20 +39,19 @@ public class RessourceDAOImpl implements IRessourceDAO {
     @Override
     public List<Ressource> findAll() {
         List<Ressource> ressources = new ArrayList<>();
-        try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ressource");
+        String query = "SELECT * FROM ressource";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                Ressource ressource = new Ressource();
-                ressource.setId_ressource(rs.getInt("id_ressource"));
-                ressource.setNom(rs.getString("nom"));
-                ressource.setType(rs.getString("type"));
-                ressource.setQuantite(rs.getInt("quantite"));
-                ressource.setFournisseur(rs.getString("fournisseur"));
-                ressources.add(ressource);
+                Ressource r = new Ressource();
+                r.setId_ressource(rs.getInt("id_ressource"));
+                r.setNom(rs.getString("nom"));
+                r.setType(rs.getString("type"));
+                r.setQuantite(rs.getInt("quantite"));
+                r.setFournisseur(rs.getString("fournisseur"));
+                r.setId_tache(rs.getInt("id_tache"));
+                ressources.add(r);
             }
-            rs.close();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -58,38 +60,37 @@ public class RessourceDAOImpl implements IRessourceDAO {
 
     @Override
     public Ressource getRessource(int id) {
-        Ressource ressource = null;
-        try {
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM ressource WHERE id_ressource = ?");
+        Ressource r = null;
+        String query = "SELECT * FROM ressource WHERE id_ressource=?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                ressource = new Ressource();
-                ressource.setId_ressource(rs.getInt("id_ressource"));
-                ressource.setNom(rs.getString("nom"));
-                ressource.setType(rs.getString("type"));
-                ressource.setQuantite(rs.getInt("quantite"));
-                ressource.setFournisseur(rs.getString("fournisseur"));
+                r = new Ressource();
+                r.setId_ressource(rs.getInt("id_ressource"));
+                r.setNom(rs.getString("nom"));
+                r.setType(rs.getString("type"));
+                r.setQuantite(rs.getInt("quantite"));
+                r.setFournisseur(rs.getString("fournisseur"));
+                r.setId_tache(rs.getInt("id_tache"));
             }
-            rs.close();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return ressource;
+        return r;
     }
 
     @Override
     public Ressource update(Ressource r) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("UPDATE ressource SET nom=?, type=?, quantite=?, fournisseur=? WHERE id_ressource=?");
+        String query = "UPDATE ressource SET nom=?, type=?, quantite=?, fournisseur=?, id_tache=? WHERE id_ressource=?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setString(1, r.getNom());
             ps.setString(2, r.getType());
             ps.setInt(3, r.getQuantite());
             ps.setString(4, r.getFournisseur());
-            ps.setInt(5, r.getId_ressource());
+            ps.setInt(5, r.getId_tache());
+            ps.setInt(6, r.getId_ressource());
             ps.executeUpdate();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -98,13 +99,13 @@ public class RessourceDAOImpl implements IRessourceDAO {
 
     @Override
     public void deleteRessource(int id) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("DELETE FROM ressource WHERE id_ressource = ?");
+        String query = "DELETE FROM ressource WHERE id_ressource=?";
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setInt(1, id);
             ps.executeUpdate();
-            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 }
+
---
+++ ConstructionXpert_Services_Solution/src/main/webapp/afficherRessource.jsp
@@ -0,0 +1,68 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="metier.Ressource" %>
+<%@ page import="dao.RessourceDAOImpl" %>
+<%@ page import="metier.Tache" %>
+<%@ page import="dao.TacheDAOImpl" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Gérer les Ressources</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<%
+    // Vérifier si le paramètre "idTache" est présent et non null dans la requête
+    String idTacheParam = request.getParameter("idTache");
+    int idTache = 0; // Valeur par défaut si le paramètre est manquant ou null
+    if (idTacheParam != null && !idTacheParam.isEmpty()) {
+        idTache = Integer.parseInt(idTacheParam);
+    }
+
+    // Si le paramètre "idTache" est égal à zéro, vous pouvez choisir de gérer ce cas selon votre logique métier
+
+    TacheDAOImpl tacheDAO = new TacheDAOImpl();
+    Tache tache = tacheDAO.getTache(idTache);
+
+    RessourceDAOImpl ressourceDAO = new RessourceDAOImpl();
+    List<Ressource> ressources = ressourceDAO.findAll();
+
+%>
+<div class="container">
+    <h1 class="mt-5">Gérer les Ressources pour la tâche: <%= tache != null ? tache.getDescription() : "Tâche non trouvée" %></h1>
+    <a href="addRessource.jsp?idTache=<%= idTache %>" class="btn btn-primary mb-3">Ajouter Ressource</a>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Type</th>
+            <th scope="col">Quantité</th>
+            <th scope="col">Fournisseur</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <% for (Ressource ressource : ressources) { %>
+        <tr>
+            <th scope="row"><%= ressource.getId_ressource() %></th>
+            <td><%= ressource.getNom() %></td>
+            <td><%= ressource.getType() %></td>
+            <td><%= ressource.getQuantite() %></td>
+            <td><%= ressource.getFournisseur() %></td>
+            <td>
+                <a href="editRessource.jsp?idRessource=<%= ressource.getId_ressource() %>" class="btn btn-warning btn-sm">Modifier</a>
+                <a href="deleteRessource.jsp?idRessource=<%= ressource.getId_ressource() %>" class="btn btn-danger btn-sm">Supprimer</a>
+            </td>
+        </tr>
+        <% } %>
+        </tbody>
+    </table>
+</div>
+
+</body>
+</html>
+
